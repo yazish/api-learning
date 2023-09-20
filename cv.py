86b72176771a923f0f7894013d60cf533a5d7f14
Yazish Kavina <yazishkavina@gmail.com>
2023-09-21 04:21:19 +0530

    commited in canada 1

---
+++ cv.py
@@ -0,0 +1,7 @@
+import cv2 as cv
+
+img = cv.imread("C:\Users\Yazish\Documents\API-learning\react-complete-guide-code\selecting-a-branch.jpg")
+cv.imshow("Yaz", img)
+cv.waitKey(0)
+
+
--- forLoop.py
+++ forLoop.py
@@ -1,4 +1,7 @@
 for a in range(10):
     print(a)
 
+
 # this is a comment for master branch
+
+
